1ea9c41fc1f0ab99337e23eb4ae037cf1b75c4b4
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-06-17 21:03:33 +0900

    Add Noteitem/style.js

---
+++ src/components/Noteitem/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 500px;
+    height: 280px;
+    background-color: #e9ecef;
+    margin: 0 auto;
+    margin-top: 40px;
+    border-radius: 50px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
