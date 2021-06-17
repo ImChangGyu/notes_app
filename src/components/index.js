7359745235fe809c7a7867c76ce57dfde9fbcd40
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-06-17 21:00:50 +0900

    Add index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header/Header";
+import Write from "./Write/Write";
+import List from "./List/List";
+import Noteitem from "./Noteitem/Noteitem";
+
+export { Header, Write, List, Noteitem };
