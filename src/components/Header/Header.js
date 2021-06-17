09b06178f9931145c7bee3191c0e3bdf9f3b5554
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-06-17 21:02:12 +0900

    Add Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./style";
+
+const Header = () => {
+    return (
+        <>
+            <h1>Notes App</h1>
+            <S.DefaultBtn>Create Note</S.DefaultBtn>
+        </>
+    );
+};
+
+export default Header;
