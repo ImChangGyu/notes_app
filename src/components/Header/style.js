00a01180141c821407cb8b876f8d005148b39ba8
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-06-17 21:02:49 +0900

    Add Header/style.js

---
+++ src/components/Header/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const DefaultBtn = styled.a`
+    cursor: pointer;
+    width: 140px;
+    height: 50px;
+    background-color: #868e96;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    border-radius: 50px;
+    box-shadow: 1px 1px 1px 1px gray;
+`;
